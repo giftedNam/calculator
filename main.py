be8dddeaacf44c005ddda8c0ca4b6b1d78b7318a
giftedNam <llokip88@gmail.com>
2023-01-18 12:36:14 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
+
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -13,13 +14,21 @@ class Calculator(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
-        self.btn1=QPushButton('Message',self)
+        self.btn1=QPushButton('Message',self) # 버튼 1
         self.btn1.clicked.connect(self.activateMessage)
+
+        self.btn2=QPushbutton('Clear',self) # 버튼 2: 내역 지우기
+        self.btn2.clicked.connet(se;f/clearMessage)
+
+        hbox=QHBoxLayout() # 수평박스 레이아웃을 추가하고 버튼 1, 2 추가
+        hbox.addStretch(1) # 공백
+        hbox.addWidget(self.btn1) # 버튼 1 배치
+        hbox.addWidget(self.btn2) # 버튼 2 배치
 
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addStretch(1)
-        vbox.addWidget(self.btn1)
+        #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox) # btn1 위치에 hbox를 배치
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -32,6 +41,9 @@ class Calculator(QWidget):
     def activateMessage(self):
         #QMessageBox.information(self,"information","Button clicked!")
         self.te1.appendPlainText("Button Clicked!")
+
+    def clearMessage(self):
+        self.te1.clear()
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
